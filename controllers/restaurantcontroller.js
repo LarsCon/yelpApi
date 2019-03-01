8a601a24b68a85971770ab39b4e0d3bd134be3d3
Lars Conard <lars.conard@gmail.com>
2019-02-28 20:35:08 -0500

    Update

--- controllers/restaurantcontroller.js
+++ controllers/restaurantcontroller.js
@@ -5,7 +5,8 @@ const fetch = require('node-fetch');
 router.get('/', async (req, res) => {
   const apiBaseUrl = process.env.YELP_URL;
   const apiKey = process.env.API_KEY;
-  const request = await fetch(apiBaseUrl + 'businesses/search?location=indianapolis', {
+  const {cost, state} =  req.query;
+  const request = await fetch(apiBaseUrl + `businesses/search?location=${state}&cost=${cost}`, {
     method: 'GET',
     headers: {
       Accept: 'application/json',
@@ -13,7 +14,7 @@ router.get('/', async (req, res) => {
     }
   });
   const places = await request.json();
-  res.status(200).json({ places });
+  res.status(200).json(places.businesses);
 });
 
 module.exports = router;
